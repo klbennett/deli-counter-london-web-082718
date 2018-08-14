70e1e13f95e7f35ba377687e1df07ff6ea18eea6
Kay B <kaylouisebennett@gmail.com>
2018-08-14 23:57:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
 def line(katz_deli)
-  current_line = ["The line is currently: "]
+  current_line = "The line is currently: "
   if katz_deli.length == 0 
     puts "The line is currently empty."
  else
